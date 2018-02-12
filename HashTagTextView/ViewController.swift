93dd973c95158b70e6d0891a0e9ee9d0b0306cc1
kz56cd <kz56cd@gmail.com>
2018-02-12 19:23:27 +0900

    Update textview (to linked hashtag)

--- HashTagTextView/ViewController.swift
+++ HashTagTextView/ViewController.swift
@@ -11,9 +11,31 @@ import RegeributedTextView
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textView: RegeributedTextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        prepareTextView()
     }
-    
 }
 
+extension ViewController {
+    fileprivate func prepareTextView() {
+        textView.delegate = self
+        textView.addAttribute(
+            .hashTag,
+            attribute: .textColor(.yellow)
+        )
+    }
+}
+
+extension ViewController: RegeributedTextViewDelegate {
+    func regeributedTextView(
+        _ textView: RegeributedTextView,
+        didSelect text: String,
+        values: [String : Any]
+        ) {
+        
+        print("👾 selected hashtag: \(text)")
+    }
+}
